e633a8a6a7f18883e0a1c06594a214127750e777
djyuvrajjaipur <djyuvrajinfo@gmail.com>
2026-09-06 08:09:39 +0530

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-const menuBtn=document.querySelector('.menu-btn'),nav=document.querySelector('.nav-menu');
-if(menuBtn)menuBtn.addEventListener('click',()=>nav.classList.toggle('open'));
-document.querySelectorAll('.nav-menu a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')));
